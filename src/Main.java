3fe5e389b26417547a4c167de3b9a18274b178de
poliakovak <kseniiapoliakova01@gmail.com>
2022-02-20 11:53:51 +0200

    Add description

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My super app!");
-
+        System.out.println("App calculator");
         int a = 5;
         int b = 7;
         int c = add(a, b);
