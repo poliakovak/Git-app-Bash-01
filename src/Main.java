8b5a365c18388e71711a70973dae28e2c7921b81
poliakovak <kseniiapoliakova01@gmail.com>
2022-02-20 10:55:52 +0200

    Add addition in class Main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My git app. Super!");
+
+        int a = 5;
+        int b = 7;
+        int c = add(a, b);
+        System.out.println(c);
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
     }
 }
