f9b135a218161c91489384a1944f173beadb60ce
poliakovak <kseniiapoliakova01@gmail.com>
2022-02-22 11:04:55 +0200

    Add division in class Main

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,18 @@ public class Main {
 
         System.out.println("App calculator");
         System.out.println("Version 1.0");
+
         int a = 5;
         int b = 7;
+
         int c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
         System.out.println(d);
         int e = subtract(a, b);
         System.out.println(e);
+        int f = divide(a, b);
+        System.out.println(f);
     }
 
     private static int add(int a, int b) {
@@ -24,5 +28,9 @@ public class Main {
 
     private static int subtract(int a, int b) {
         return a - b;
+    }
+
+    private static int divide(int a, int b) {
+        return a / b;
     }
 }
